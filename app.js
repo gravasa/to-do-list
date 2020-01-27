a7598b30473ffebf43e1796a81e90f47efa04ee9
Alex G <alexgavras@Alex-MacBook-Air.local>
2020-01-27 22:15:52 +0700

    Get rid of space in the mongodb link

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-alex:avgyst@cluster0-t2prz.mongodb.net /todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-alex:avgyst@cluster0-t2prz.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
